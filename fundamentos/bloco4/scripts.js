05c1f396a478fa17efd88f798c72540ce3256015
Ana Paula Aguiar <paulaaguiars16@gmail.com>
2022-08-18 19:34:26 -0300

    adiciona arquivos de estudos js

---
+++ fundamentos/bloco4/scripts.js
@@ -0,0 +1,7 @@
+const myName = "Ana Paula";
+const birthCity = "Teresina";
+let birthYear = 1997;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
---
+++ fundamentos/bloco4/switch-case.js
@@ -0,0 +1,15 @@
+let pessoa = 'lista';
+
+switch (pessoa) {
+    case 'aprovada':
+        console.log('Aprovada');
+        break;
+    case 'lista':
+        console.log('Lista');
+        break;
+    case 'reprovada':
+        console.log('reprovada');
+        break;
+    default:
+        console,log('nao se aplica')
+}
