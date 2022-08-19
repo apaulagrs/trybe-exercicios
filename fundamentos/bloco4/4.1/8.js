d294f98bccc583751a535f908b01c2f957b86610
Ana Paula Aguiar <paulaaguiars16@gmail.com>
2022-08-18 21:30:19 -0300

    adiciona exercicios 4.1

---
+++ fundamentos/bloco4/4.1/8.js
@@ -0,0 +1,12 @@
+const a = 5;
+const b = 30;
+const c = 9;
+
+if ((a % 2) === 0 || (b % 2) === 0  || (c % 2) === 0) {
+    console.log('true');
+}
+else {
+    console.log('false');
+}
+
+// (num % 2) === 0; par
