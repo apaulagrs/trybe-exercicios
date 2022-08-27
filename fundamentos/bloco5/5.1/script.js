3872cc6cd1a83d13120a5f10b72c71ac313e0778
Ana Paula Aguiar <paulaaguiars16@gmail.com>
2022-08-26 21:26:56 -0300

    Adiciona administrador de tempo da trybe

---
+++ fundamentos/bloco5/5.1/script.js
@@ -0,0 +1,10 @@
+document.getElementById("header-container").style.backgroundColor = "green";
+document.getElementById("footer-container").style.backgroundColor = "darkgreen";
+document.getElementsByClassName("emergency-tasks")[0].style.backgroundColor = "salmon";
+document.getElementsByClassName("no-emergency-tasks")[0].style.backgroundColor = "orange";
+document.getElementsByTagName("h3")[0].style.backgroundColor = "purple";
+document.getElementsByTagName("h3")[1].style.backgroundColor = "purple";
+document.getElementsByTagName("h3")[2].style.backgroundColor = "black";
+document.getElementsByTagName("h3")[3].style.backgroundColor = "black";
+
+
