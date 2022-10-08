692a4ab1f7e2257e2a99f664daeb012a3da77cb8
Ana Paula Aguiar <paulaaguiars16@gmail.com>
2022-10-08 07:05:00 -0300

    Organizando diretórios

---
+++ front-end/sessao1/1.1/push-number-async.js
@@ -0,0 +1,27 @@
+// pushNumberAsync.js
+
+const pushNumber = (list, number) => list.push(number);
+
+const numbers = [];
+
+/* pushNumber(numbers, 1);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+console.log(numbers); */
+
+// Saída:
+// [ 1, 2, 3 ]
+
+// pushNumberAsync.js
+
+const THREE_SECONDS = 3000;
+
+setTimeout(() => pushNumber(numbers, 1), THREE_SECONDS);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+setTimeout(() => console.log(numbers), THREE_SECONDS);
+
+// Saída:
+// [2, 3]
