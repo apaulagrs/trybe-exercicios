238c771e06b5d77224df02f57f434d94bbfc6f6b
Ana Paula Aguiar <paulaaguiars16@gmail.com>
2022-10-20 12:24:23 -0300

    Exercicios de fixacao React my-app

---
+++ front-end/sessao2/2.1/first-react/src/About.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class MyHabilities extends Component {
+    render() {
+    return (
+        <div>
+            <h1> Ana Paula </h1>
+                <p>Tenho 25 anos, moro em Timon - MA. Formada em Ciencias Sociais, estudante de Desenvolvimento Web na Trybe e aprendendo o React!</p>
+            <h2>Minhas habilidades</h2>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>Jest</li>
+                    <li>React</li>
+                </ul>
+
+        </div>
+
+        )
+    }
+}
+
+export default MyHabilities;
