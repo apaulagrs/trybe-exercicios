f366caae37fe96383b145fbf0db88565889a1c14
Ana Paula Aguiar <paulaaguiars16@gmail.com>
2023-03-28 20:49:52 -0300

    exercicios 5

--- backend/sessao4/4.1/exercicios/my-scripts/bmi.js
+++ backend/sessao4/4.1/exercicios/my-scripts/bmi.js
@@ -1,8 +1,37 @@
-const readline = require('readline-sync');
+const readlineSync = require('readline-sync');
 
-const weight = 76;
-const height = 175;
+const weightQuestion = readlineSync.questionFloat("What's your weight? (kg) ");
+const heightQuestion = readlineSync.questionInt("What's your height? (cm) ");
 
-const bmi = weight / (height * height);
+const categoryBMI = (bmi) => {
+    if (bmi <= 18.5) {
+        console.log('Abaixo do peso (magreza)');
+    } else if (bmi >= 18.5 && bmi < 25) {
+        console.log('Peso normal');
+    } else if (bmi >= 25 && bmi < 30) {
+        console.log('Acima do peso (sobrepeso)');
+    } else if (bmi >= 30 && bmi < 35) {
+        console.log('Obesidade grau I');
+    } else if (bmi >= 35 && bmi < 40) {
+        console.log('Obesidade grau II');
+    } else {
+        console.log('Obesidade graus III e IV')
+    }
+}
 
-console.log(bmi);
+function handleBMI(weight, height) {
+    console.log(`Weight: ${weight}, Height: ${height}`);
+
+    const heightMts = height / 100;
+    const heightSqd = heightMts ** 2;
+
+    const bmi = weight / heightSqd;
+    return bmi;
+};
+
+function main() {
+    const bmi = handleBMI(weightQuestion, heightQuestion);
+    console.log(`BMI: ${bmi.toFixed(2)}. Categoria: ${categoryBMI(bmi)}`);
+}
+
+main();
