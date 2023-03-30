34e862224af322cca004caf89c654c48bcaec7e2
Ana Paula Aguiar <paulaaguiars16@gmail.com>
2023-03-30 14:48:23 -0300

    exercicio 6A

---
+++ backend/sessao4/4.1/exercicios/simpsons/simpsons.js
@@ -0,0 +1,14 @@
+const fs = require('fs').promises;
+
+async function readAll() {
+  const fileContent = await fs.readFile('./simpsons.json', 'utf-8');
+  const simpsons = JSON.parse(fileContent);
+  const strings = simpsons.map(({ id, name }) => `${id} - ${name}`);
+  strings.forEach(e => console.log(e));
+};
+
+async function main() {
+  await readAll();
+};
+
+main();
