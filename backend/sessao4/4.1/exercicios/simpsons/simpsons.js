0b821bd522d5ea562773650ddd377f28797f47d8
Ana Paula Aguiar <paulaaguiars16@gmail.com>
2023-03-30 16:15:50 -0300

    exercicio 6b

--- backend/sessao4/4.1/exercicios/simpsons/simpsons.js
+++ backend/sessao4/4.1/exercicios/simpsons/simpsons.js
@@ -1,5 +1,17 @@
 const fs = require('fs').promises;
 
+async function getSimpsonById(id) {
+  const fileContent = await fs.readFile('./simpsons.json', 'utf-8');
+  const simpsons = JSON.parse(fileContent);
+  const choosenSimpson = simpsons.find((simpson) => +simpson.id === id)
+
+  if (!choosenSimpson) {
+    throw new Error('Id não encontrado');
+  }
+
+  return choosenSimpson;
+}
+
 async function readAll() {
   const fileContent = await fs.readFile('./simpsons.json', 'utf-8');
   const simpsons = JSON.parse(fileContent);
@@ -8,7 +20,8 @@ async function readAll() {
 };
 
 async function main() {
-  await readAll();
+  const simpson = await getSimpsonById(1);
+  console.log(simpson);
 };
 
 main();
