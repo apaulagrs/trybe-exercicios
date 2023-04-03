1a5c2c2434842f34ec62aaa5506ef37ce62a2bec
Ana Paula Aguiar <paulaaguiars16@gmail.com>
2023-04-03 19:18:28 -0300

    fix: reparos em validateTeam

--- backend/sessao4/4.4/soccer-team-manager/src/middlewares/validateTeam.js
+++ backend/sessao4/4.4/soccer-team-manager/src/middlewares/validateTeam.js
@@ -1,9 +1,13 @@
-const validateTeam = (req, res) => {
+const validateTeam = (req, res, next) => {
   const { nome, sigla } = req.body;
-  if (!nome) return res.status(400)
-    .json({ message: 'O campo "nome" é obrigatório '});
-  if (!sigla) return res.status(400)
-    .json({ message: 'O campo "sigla" é obrigatório '});
+  if (!nome) {
+ return res.status(400)
+    .json({ message: 'O campo "nome" é obrigatório ' }); 
+}
+  if (!sigla) {
+ return res.status(400)
+    .json({ message: 'O campo "sigla" é obrigatório ' }); 
+}
   next();
 };
 
