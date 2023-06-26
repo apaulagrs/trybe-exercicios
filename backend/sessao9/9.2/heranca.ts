5ecc7192f2bd353ffca56547e48235324850068f
Ana Paula Aguiar <paulaaguiars16@gmail.com>
2023-06-25 22:59:31 -0300

    feat: composicao

--- backend/sessao9/9.2/heranca.ts
+++ backend/sessao9/9.2/heranca.ts
@@ -1,5 +1,31 @@
-class Animal {
-  constructor(public name:string, protected birthDate:Date) { }
+interface Animal {
+  name: string;
+  age: number;
+
+  getBirthDate(): Date;
+}
+
+// class Animal {
+//   constructor(public name:string, protected birthDate:Date) { }
+
+//   get ages() {
+//     const timeDiff = Math.abs(
+//       Date.now() - new Date(this.birthDate).getTime()
+//     )
+//     return Math.floor(timeDiff / (1000 * 3600 * 24) / 365.25);
+//   }
+// }
+
+// class Mammal extends Animal {
+//   walk() {
+//     console.log(`${this.name} está andando!`);
+//   }
+// }
+
+class Bird implements Animal {
+  constructor(
+    public name:string,
+    private birthDate:Date) {}
 
   get age() {
     const timeDiff = Math.abs(
@@ -7,40 +33,36 @@ class Animal {
     )
     return Math.floor(timeDiff / (1000 * 3600 * 24) / 365.25);
   }
-}
 
-class Mammal extends Animal {
-  walk() {
-    console.log(`${this.name} está andando!`);
+  getBirthDate(): Date {
+    return this.birthDate;
   }
-}
 
-class Bird extends Animal {
   fly() {
     console.log(`${this.name} está voando!`);
   }
 
-  showBirthDate() {
-    console.log(this.birthDate);
-  }
+  // showBirthDate() {
+  //   console.log(this.birthDate);
+  // }
 }
 
-const tiger = new Mammal(
-  'Tigre',
-  new Date(Date.parse('May 03, 2020')),
-);
+// const tiger = new Mammal(
+//   'Tigre',
+//   new Date(Date.parse('May 03, 2020')),
+// );
 
-const main = (animal:Animal) => {
-  console.log(animal.age);
-};
+// const main = (animal:Animal) => {
+//   console.log(animal.age);
+// };
+
+// main(tiger);
+// tiger.walk();
 
 const parrot = new Bird(
   'Papagaio',
   new Date('Jun 07, 2017'),
 );
-
-main(tiger);
-tiger.walk();
 
 console.log(parrot.age);
 parrot.fly();
--- backend/sessao9/9.2/para-fixar.ts
+++ backend/sessao9/9.2/para-fixar.ts
@@ -7,17 +7,36 @@ class Superclass {
 }
 
 class Subclass extends Superclass {
-  constructor(isSuper:boolean = true) {
+  constructor() {
     super();
+    this.isSuper = false;
   }
 }
 
 const myFunc = (value:Superclass) => {
-  return value.sayHello();
+  console.log(value.isSuper ? 'Super!' : 'Sub!');
 };
 
-const sub1 = new Subclass(false);
-const super1 = new Superclass(true);
+const sub1 = new Subclass();
+const super1 = new Superclass();
 
-myFunc(sub1);
-myFunc(super1);
+// myFunc(sub1);
+// myFunc(super1);
+
+interface MyInterface{
+  myNumber: number;
+  myFunc(myParam:number):string;
+}
+
+class MyClass implements MyInterface {
+  constructor(public myNumber:number = 0) { }
+
+  myFunc(myParam: number): string {
+    return `Soma: ${myParam + this.myNumber}`
+  }
+}
+
+const somas = new MyClass(26);
+
+console.log(somas.myNumber);
+console.log(somas.myFunc(4));
