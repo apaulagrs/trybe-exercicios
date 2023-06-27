49d05b9593f40d10454509754bf60b4167b19048
Ana Paula Aguiar <paulaaguiars16@gmail.com>
2023-06-26 22:08:19 -0300

    feat: single-responsability

--- backend/sessao9/9.4/single-responsability/src/index.ts
+++ backend/sessao9/9.4/single-responsability/src/index.ts
@@ -7,38 +7,94 @@ type Discipline = {
 type Student = {
   name:string;
   disciplines:Discipline[];
+  school?:string;
 }
 
-function setApproved(students:Array<Student>) {
-  const studentWithLetterGrade = students.map((s) => {
-    const disciplinesWithLetterGrade = s.disciplines.map((d) => {
-      if (d.grade >= 0.9) d.letterGrade = 'A';
-      else if (d.grade >= 0.8) d.letterGrade = 'B';
-      else if (d.grade >= 0.7) d.letterGrade = 'C';
-      else if (d.grade >= 0.6) d.letterGrade = 'D';
-      else if (d.grade >= 0.1) d.letterGrade = 'E';
-      else d.letterGrade = 'F'
+// function setApproved(students:Array<Student>) {
+//   const studentWithLetterGrade = students.map((student) => {
+//     const disciplinesWithLetterGrade = student.disciplines.map((discipline) => {
+//       if (discipline.grade >= 0.9) discipline.letterGrade = 'A';
+//       else if (discipline.grade >= 0.8) discipline.letterGrade = 'B';
+//       else if (discipline.grade >= 0.7) discipline.letterGrade = 'C';
+//       else if (discipline.grade >= 0.6) discipline.letterGrade = 'D';
+//       else if (discipline.grade >= 0.1) discipline.letterGrade = 'E';
+//       else discipline.letterGrade = 'F';
+//       return discipline;
+//     });
+//     return {
+//       name: student.name,
+//       disciplines: disciplinesWithLetterGrade,
+//     };
+//   });
+//   const approvedStudents = studentWithLetterGrade.filter(({ disciplines }) => {
+//     disciplines.every((discipline) => discipline.grade > 0.7);
+//   });
+//   approvedStudents.map(({ name, disciplines }) => {
+//     console.log(`A pessoa com nome ${name} foi aprovada!`);
+//     disciplines.map(({ name, letterGrade }) => console.log(`${name}: ${letterGrade}`));
+//   });
+// }
 
-      return d;
-    });
-
-    return {
-      name: s.name,
-      disciplines: disciplinesWithLetterGrade,
-    };
-  });
-
-  const approvedStudents = studentWithLetterGrade.filter(({ disciplines }) => {
-    disciplines.every((d) => d.grade > 0.7);
-  });
-
-  approvedStudents.map(({ name, disciplines }) => {
-    console.log(`A pessoa com nome ${name} foi aprovada!`);
-    disciplines.map(({ name, letterGrade }) => console.log(`${name}: ${letterGrade}`));
-  });
+const GRADE_DICT = {
+  numbers: [0.9, 0.8, 0.7, 0.6, 0.1],
+  letters: ['A', 'B', 'C', 'D', 'E'],
 };
 
-const studentsA = [
+const getGradeLetter = (gradeNumber:number):string => {
+  const gradeNumbers = GRADE_DICT.numbers;
+  const gradeLetters = GRADE_DICT.letters;
+  for (let i = 0; i < gradeNumbers.length; i += 1) {
+    if (gradeNumber >= gradeNumbers[i]) return gradeLetters[i];
+  }
+  return 'F';
+};
+
+const getLetterGrades = (discipline:Discipline):Discipline => ({
+  ...discipline,
+  letterGrade: getGradeLetter(discipline.grade),
+});
+
+const percentageGradesIntoLetters = (student:Student):Student => ({
+  ...student,
+  disciplines: student.disciplines.map(getLetterGrades),
+});
+
+// const percentageGradesIntoLetters = ({ name: stendtName, disciplines }: Student):
+//   {name:string, disciplines:Discipline[]} => ({
+//     name: stendtName,
+//     // eslint-disable-next-line complexity, sonarjs/cognitive-complexity
+//     disciplines: disciplines.map(({ name, grade }) => {
+//       let letterGrade;
+
+//       if (grade >= 0.9) letterGrade = 'A';
+//       else if (grade >= 0.8) letterGrade = 'B';
+//       else if (grade >= 0.7) letterGrade = 'C';
+//       else if (grade >= 0.6) letterGrade = 'D';
+//       else if (grade >= 0.1) letterGrade = 'E';
+//       else letterGrade = 'F';
+
+//       return { name, grade, letterGrade };
+//     }),
+// });
+
+const approvedStudents = ({ disciplines }:Student):boolean => disciplines.every(
+  ({ grade }) => grade > 0.7,
+);
+
+const updateApprovalData = (student:Student):void => {
+  console.log(`A pessoa com nome ${student.name} foi aprovada!`);
+  student.disciplines.forEach(({ name, letterGrade }) =>
+    console.log(`${name}: ${letterGrade}`));
+};
+
+function setApproved(students:Student[]):void {
+  students
+    .map(percentageGradesIntoLetters)
+    .filter(approvedStudents)
+    .map(updateApprovalData);
+}
+
+const students = [
   {
     name: 'Marx',
     disciplines: [
@@ -55,4 +111,12 @@ const studentsA = [
   },
 ];
 
-setApproved(studentsA);
+setApproved(students);
+
+export {
+  percentageGradesIntoLetters,
+  approvedStudents,
+  updateApprovalData,
+  setApproved,
+  getLetterGrades,
+};
