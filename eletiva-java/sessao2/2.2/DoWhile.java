9b5c05a71cc478e244b5ce3c671dab8397ad86cf
Ana Paula Aguiar <paulaaguiars16@gmail.com>
2023-09-06 21:59:03 -0300

    java: for, while & doWhile

---
+++ eletiva-java/sessao2/2.2/DoWhile.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class DoWhile {
+
+  public static void main(String[] args) {
+    int tentativas = 1;
+    do {
+      System.out.println("Tentando... ");
+      tentativas++;
+    } while(tentativas == 3);
+  }
+}
