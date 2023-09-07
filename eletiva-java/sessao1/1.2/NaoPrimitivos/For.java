b14e496eb4f3a4ccf69bbb6eb389ac637a4f620a
Ana Paula Aguiar <paulaaguiars16@gmail.com>
2023-09-07 20:33:09 -0300

    indexOf, split, toUpperCase

---
+++ eletiva-java/sessao1/1.2/NaoPrimitivos/For.java
@@ -0,0 +1,13 @@
+public class For {
+
+  public static void main(String[] args) {
+    int number;
+    String log;
+
+    for (number = 1; number <=10; number += 1) {
+      log = "O número atual é: " + number + "\n";
+
+      System.out.println(log);
+    }
+  }
+}
--- eletiva-java/sessao1/1.3/IndexOf.java
+++ eletiva-java/sessao1/1.3/IndexOf.java
@@ -1,7 +1,6 @@
 public class IndexOf {
 
   public static void main(String[] args) {
-    String saudacao = "Olá, mundo!";
     String lorem = "Lorem ipsum dolor sit amet";
 
     System.out.println(lorem.indexOf("s", 10));
